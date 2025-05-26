8991ca285f09072960b030b220c46f56ad30761a
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-05-26 14:45:43 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,13 +5,13 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../repositories/website_tokasafe/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/../repositories/toka_safe/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../repositories/website_tokasafe/vendor/autoload.php';
+require __DIR__ . '/../repositories/toka_safe/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../repositories/website_tokasafe/bootstrap/app.php')
+(require_once __DIR__ . '/../repositories/toka_safe/bootstrap/app.php')
     ->handleRequest(Request::capture());
